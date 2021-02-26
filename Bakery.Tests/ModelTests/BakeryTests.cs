a8af86f845b2e7c20a87cfe962584b34a13146da
Dani Renner <danijrenner@gmail.com>
2021-02-26 09:33:04 -0800

    bread and pastry constructor tests pass

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -1,3 +1,23 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Menu.Models;
 
+namespace Menu.TestTools
+{
+  [TestClass]
+  public class MenuTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceofBread_Bread()
+    {
+      Bread newBread = new Bread(2);
+      Assert.AreEqual(typeof(Bread),newBread.GetType());
+    }
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceofPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(3);
+      Assert.AreEqual(typeof(Pastry),newPastry.GetType());
+    }
+  }
+}
--- Bakery/Models/Menu.cs
+++ Bakery/Models/Menu.cs
@@ -3,9 +3,17 @@ namespace Menu.Models
   public class Bread
   {
     public int BreadCount{get; set;}
+    public Bread(int breadcount)
+    {
+      BreadCount = breadcount;
+    }
   }
   public class Pastry
   {
     public int PastryCount{get; set;}
+    public Pastry(int pastrycount)
+    {
+      PastryCount = pastrycount;
+    }
   }
 }
